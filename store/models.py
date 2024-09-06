c6b0e71f7c096248553c861e26cfa2f3c238e533
Mopano <p.r.7.norouzi@gmail.com>
2024-09-06 05:03:17 -0700

    feature: added one to one

--- store/models.py
+++ store/models.py
@@ -9,9 +9,36 @@ class Product(models.Model) :
     last_update = models.DateTimeField(auto_now= True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOISES = [
+        (MEMBERSHIP_GOLD, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Solver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique= True)
     phone = models.CharField(max_length=12)
     birth_date = models.DateField(null=True)
 
+class Order(models.Model):
+    PAYMENT_PENDING = "P"
+    PAYMENT_COMPLETE = "C"
+    PAYMENT_FAILED = "F"
+
+    PAYMENT_STATUS = [
+        (PAYMENT_COMPLETE, "Complete"),
+        (PAYMENT_PENDING, "Pending"),
+        (PAYMENT_FAILED, "failed")
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1,  choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
