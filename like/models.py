faf83acb60cad63b94321051f894dad8187083f1
Mopano <p.r.7.norouzi@gmail.com>
2024-09-09 02:04:50 -0700

    feature : Generic Relationships

--- like/models.py
+++ like/models.py
@@ -1,13 +1,11 @@
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
-class Tag(models.Model):
-    tag = models.CharField(max_length=255)
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete= models.CASCADE)
+class LikedItem(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_id = models.PositiveSmallIntegerField()
     content_object = GenericForeignKey()
