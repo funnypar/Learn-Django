a849fe2e976c698e80c8f459aa9377895a7ad5bd
Mopano <99732517+funnypar@users.noreply.github.com>
2024-09-22 03:16:35 -0700

    added admin

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Order, OrderItem, Product
 
-def hello(request):
-    return HttpResponse('Hello World')
+
+def say_hello(request):
+    products = Product.objects.filter(unit_price__range=(20,30))
+    # ordered = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+    Order.objects.select_related('customer').prefetch_related('orderitem_set').order_by('-placed-at')[:5]
+    ordered = Product.objects.filter(OrderItem.objects.select_related('order__customer')[:5]).order_by('title')
+    return render(request, 'hello.html', {'name': 'Mosh', "products":products})
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_per_page = 10
+    list_editable = ['unit_price']
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ['membership']
+    list_per_page = 10
+
+admin.site.register(models.Collection)
+
