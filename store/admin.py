5d19ee2103a1712bee25879f900bf0909feb6f2f
Mopano <99732517+funnypar@users.noreply.github.com>
2024-09-22 03:22:59 -0700

    feature: added computed inventory

--- store/admin.py
+++ store/admin.py
@@ -6,9 +6,16 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price','inventory_status']
     list_per_page = 10
     list_editable = ['unit_price']
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self,product):
+        if product.inventory > 10 : 
+            return "OK"
+        return "LOW"
+        
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
