2594814d2dc2d63817a371e468b635b98a107ea3
Mopano <99732517+funnypar@users.noreply.github.com>
2024-10-12 03:15:19 -0700

    feature: added router and viewsets

--- store/views.py
+++ store/views.py
@@ -1,45 +1,39 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-from .models import Collection, Product
+from .models import Collection, OrderItem, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
 
 # Products
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-    def get_serializer_context(self):
-        return {'request':self.request}
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def delete(self, request,pk):
-        product = get_object_or_404(Product, pk=pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_serializer_context(self):
+        return {'request':self.request}
 
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter( product_id = kwargs['id']).count() > 0 :
+            return Response({'error': 'Product can not be deleted ! Because that has "orderItem"'},status= status.HTTP_501_NOT_IMPLEMENTED)
+        
+        return super().destroy(request, *args, **kwargs)
+    
     
 # Collections
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter( product_id = kwargs['id']).count() > 0 :
+            return Response({'error': 'Collection can not be deleted ! Because that has "products"'},status= status.HTTP_501_NOT_IMPLEMENTED)
+        
+        return super().destroy(request, *args, **kwargs)
+        
     
 
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.all()
-    serializer_class = CollectionSerializer
-    def delete(self, request,pk):
-        collection = get_object_or_404(Collection.objects.annotate(products_count=Count('products')), pk=pk)
-        if collection.prodcuts.count() > 0 :
-            return Response({'error': 'Collection can not be deleted ! Because that has "products"'},status= status.HTTP_501_NOT_IMPLEMENTED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
+    
